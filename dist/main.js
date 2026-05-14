f02a509806d61751fc188fedf5823ea06942475a
LucasRafaelBersotLinares <lucasblinares@gmail.com>
2026-05-14 15:35:18 -0300

    Final commit

--- dist/main.js
+++ dist/main.js
@@ -12,8 +12,18 @@ const produtos = [];
 function novoProduto(req, res) {
     try {
         let data = req.body;
-        if (!data.nome || !data.fabricante || !data.preco) {
-            throw new Error("Produto requer nome, fabricante e preco");
+        let produtoDup = produtos.find(p => p.id == Number(data.id));
+        if (!data.id || !data.nome || !data.fabricante || !data.preco) {
+            throw new Error("Produto requer id, nome, fabricante e preço");
+        }
+        if (!data.fabricante.nome || !data.fabricante.endereco.cidade || !data.fabricante.endereco.pais) {
+            throw new Error("Produto requer nome de fabricante, endereco com pais válido e endereco com cidade válida");
+        }
+        if (produtoDup) {
+            throw new Error("Não é permitido ID duplicados");
+        }
+        if (!(data.preco > 0)) {
+            throw new Error("Preço tem que ser maior que zero");
         }
         const produto = new produto_1.Produto(data.id, data.nome, data.preco, data.fabricante);
         produtos.push(produto);
@@ -35,8 +45,11 @@ function listaProdutos(req, res) {
 }
 function buscarPorID(req, res) {
     try {
-        let id = req.params.id;
-        produtos.find(id);
+        let id = Number(req.params.id);
+        let produto = produtos.find(p => p.id == id);
+        if (produto) {
+            res.status(200).json({ produto });
+        }
         throw new Error("Produto não encontrado");
     }
     catch (erro) {
@@ -45,21 +58,42 @@ function buscarPorID(req, res) {
 }
 function atualizaDados(req, res) {
     try {
-        let id = req.params.id;
+        let id = Number(req.params.id);
         let data = req.body;
-        produtos.find(id => {
-            id.nome = data.nome;
-            id.preco = data.preco;
-        });
+        let produto = produtos.find(p => p.id == id);
+        if (produto) {
+            produto.id = data.id ?? produto.id;
+            produto.nome = data.nome ?? produto.nome;
+            produto.preco = data.preco ?? produto.preco;
+            produto.fabricante.nome = data.fabricante.nome ?? produto.fabricante.nome;
+            produto.fabricante.endereco.cidade = data.fabricante.endereco.cidade ?? produto.fabricante.endereco.cidade;
+            produto.fabricante.endereco.pais = data.fabricante.endereco.pais ?? produto.fabricante.endereco.pais;
+            res.status(200).json({ produto });
+        }
         throw new Error("Não achou o produto para atualizar");
     }
     catch (erro) {
         res.status(400).json({ Message: erro.message });
     }
 }
+function deletaDado(req, res) {
+    try {
+        let id = Number(req.params.id);
+        let produto = produtos.findIndex(p => p.id == id);
+        if (produto > -1) {
+            produtos.splice(produto, 1);
+            res.status(200).json({ produtos });
+        }
+        throw new Error("Produto nao encontrado");
+    }
+    catch (erro) {
+        res.status(404).json({ Message: erro.message });
+    }
+}
+app.delete('/api/produtos/delete/:id', deletaDado);
 app.put('/api/produtos/atualizar/:id', atualizaDados);
 app.get('/api/produtos/lista/:id', buscarPorID);
 app.get('/api/produtos/lista', listaProdutos);
-app.post('/api/produtos', novoProduto);
+app.post('/api/produtos/adicionar', novoProduto);
 app.listen(PORT, () => console.log("API ESTA NA PORTA: ", PORT));
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -9,13 +9,23 @@ const produtos: Produto[] = []
 function novoProduto(req: Request, res: Response){
     try {
         let data: any = req.body
+        let produtoDup = produtos.find(p => p.id == Number(data.id))
 
-        if(!data.nome || !data.fabricante || !data.preco) {
-            throw new Error("Produto requer nome, fabricante e preco")
+        if(!data.id||!data.nome || !data.fabricante || !data.preco) {
+            throw new Error("Produto requer id, nome, fabricante e preço")
+        }
+        if(!data.fabricante.nome || !data.fabricante.endereco.cidade || !data.fabricante.endereco.pais){
+            throw new Error("Produto requer nome de fabricante, endereco com pais válido e endereco com cidade válida")
+        }
+        if(produtoDup){
+            throw new Error("Não é permitido ID duplicados")
+        }
+        if(!(data.preco > 0)){
+            throw new Error("Preço tem que ser maior que zero")
         }
         const produto = new Produto(data.id,data.nome,data.preco,data.fabricante)
         produtos.push(produto)
-        res.status(200).json(produto)
+        res.status(201).json(produto)
     } catch(erro: unknown) {
         res.status(400).json({Message: (erro as Error).message})
     }
@@ -33,8 +43,11 @@ function listaProdutos(req: Request, res: Response){
 
 function buscarPorID(req: Request, res: Response){
     try {
-        let id:any = req.params.id
-        produtos.find(id)
+        let id:any = Number(req.params.id)
+        let produto = produtos.find(p => p.id == id)
+        if(produto){
+            res.status(200).json({produto})
+        }
         throw new Error("Produto não encontrado")
 
     } catch(erro: unknown){
@@ -47,18 +60,39 @@ function atualizaDados(req: Request, res: Response){
         let id:any = Number(req.params.id)
         let data:any = req.body
 
-        let produto = produtos.findIndex(p => 
-            p.id == id
-        )
-
+        let produto = produtos.find(p => p.id == id)
+        if(produto){
+            produto.id = data.id ?? produto.id
+            produto.nome = data.nome ?? produto.nome
+            produto.preco = data.preco ?? produto.preco
+            produto.fabricante.nome = data.fabricante.nome ?? produto.fabricante.nome
+            produto.fabricante.endereco.cidade = data.fabricante.endereco.cidade ?? produto.fabricante.endereco.cidade
+            produto.fabricante.endereco.pais = data.fabricante.endereco.pais ?? produto.fabricante.endereco.pais
+            res.status(200).json({produto})
+        }
         throw new Error("Não achou o produto para atualizar")
     } catch(erro: unknown){
-        res.status(400).json({Message: (erro as Error).message})
+        res.status(404).json({Message: (erro as Error).message})
     }
 }
 
+function deletaDado(req: Request, res: Response){
+    try {
+        let id = Number(req.params.id)
+        let produto = produtos.findIndex(p => p.id == id)
+        if(produto > -1){
+            produtos.splice(produto,1)
+            res.status(200).json({produtos})
+        }
+        throw new Error("Produto não encontrado")
+    } catch(erro: unknown){
+        res.status(404).json({Message: (erro as Error).message})
+    }
+}
+
+app.delete('/api/produtos/delete/:id', deletaDado)
 app.put('/api/produtos/atualizar/:id', atualizaDados)
 app.get('/api/produtos/lista/:id', buscarPorID)
 app.get('/api/produtos/lista', listaProdutos)
-app.post('/api/produtos', novoProduto)
+app.post('/api/produtos/adicionar', novoProduto)
 app.listen(PORT, () => console.log("API ESTA NA PORTA: ", PORT))
